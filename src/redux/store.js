58b6fdeeeeccbffc2e5df3efb901eb1ba3f7e019
Marius1388 <mariuscristian.bancila@gmail.com>
2020-07-22 12:12:27 +0300

    redux logger included

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,23 @@
-import {createStore,applyMiddleware, compose} from 'redux';
-import thunk from 'redux-thunk'
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 import rootReducer from './reducers';
 
 const initialState = {};
 
-const middleware = [thunk] ;
+const middleware = [thunk];
+if (process.env.NODE_ENV === 'development') {
+	middlewares.push(logger);
+}
 
-const store = createStore (rootReducer, 
-    initialState,
-    compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-        )
-    );
+const store = createStore(
+	rootReducer,
+	initialState,
+	compose(
+		applyMiddleware(...middleware),
+		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	)
+);
 
-export default store; 
+export default store;
